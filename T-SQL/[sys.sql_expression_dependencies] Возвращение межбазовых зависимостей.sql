df72c4db6654e1281e2471f6db71a8f0dc5b1fb6
Sergey Kharitonov <60604804+DevSergo@users.noreply.github.com>
2022-05-06 23:35:54 +0300

    новые примеры SYS DEPENDENCIES

---
+++ T-SQL/[sys.sql_expression_dependencies] Возвращение межбазовых зависимостей.sql
@@ -0,0 +1,21 @@
+-- Возвращение межбазовых зависимостей
+
+CREATE DATABASE db1;  
+GO  
+USE db1;  
+GO  
+CREATE PROCEDURE p1 AS SELECT * FROM db2.s1.t1;  
+GO  
+CREATE PROCEDURE p2 AS  
+    UPDATE db3..t3  
+    SET c1 = c1 + 1;  
+GO  
+SELECT OBJECT_NAME (referencing_id),referenced_database_name,   
+    referenced_schema_name, referenced_entity_name  
+FROM sys.sql_expression_dependencies  
+WHERE referenced_database_name IS NOT NULL;  
+GO  
+USE master;  
+GO  
+DROP DATABASE db1;  
+GO
